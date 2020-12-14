d41fb9da31213f14a34c0161ace3a75e47673754
sureshchaudhari13 <60111157+sureshchaudhari13@users.noreply.github.com>
2020-12-14 15:16:02 +0530

    Update code.py

--- Project-:-Car-insurance-claim/code.py
+++ Project-:-Car-insurance-claim/code.py
@@ -1,6 +1,6 @@
 # import packages
 import numpy as np
-import pandas as pd
+import pandas as  pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
